535e2758ea76b1c024d9af9a2f8a8c3d4c94770d
JuiceFN <95861553+JuiceFN@users.noreply.github.com>
2021-12-09 17:40:31 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Net;
+
+WebClient aesGrabberClient = new WebClient();
+string aesKey = aesGrabberClient.DownloadString("https://benbot.app/api/v1/aes");
+Console.WriteLine("The generated AES key is: " + aesKey);
+Console.WriteLine("USE CODE JUICEFN THIS TOOK ME ALOT OF TIME");
